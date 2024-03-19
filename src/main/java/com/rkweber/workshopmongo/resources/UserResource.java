aca03bb604b9585850d5736ee360db153fe9410c
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2024-03-19 14:39:40 -0300

    Endpoint to return a user's posts

--- src/main/java/com/rkweber/workshopmongo/resources/UserResource.java
+++ src/main/java/com/rkweber/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.rkweber.workshopmongo.domain.Post;
 import com.rkweber.workshopmongo.domain.User;
 import com.rkweber.workshopmongo.dto.UserDTO;
 import com.rkweber.workshopmongo.services.UserService;
@@ -64,6 +65,12 @@ public class UserResource { // precisa do service
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj.getPosts()); 
+	}
 	
 }
  
