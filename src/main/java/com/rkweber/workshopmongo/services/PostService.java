f18b400e5655f72dec738f585dfc0448af0d574e
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2024-03-19 16:31:13 -0300

    Simple query with query methods

--- src/main/java/com/rkweber/workshopmongo/services/PostService.java
+++ src/main/java/com/rkweber/workshopmongo/services/PostService.java
@@ -22,6 +22,6 @@ public class PostService { // precisa do repository
 		}
 	
 	public List<Post> findByTitle(String text) {
-		return repository.findByTitleContainingIgnoreCase(text);
+		return repository.searchTitle(text);
 	}
 }
