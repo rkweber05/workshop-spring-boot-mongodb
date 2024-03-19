f8c1ea330ec4e4b31caa57a790d7e17db1608e0f
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2024-03-19 14:33:21 -0300

    Referencing user posts

--- src/main/java/com/rkweber/workshopmongo/domain/User.java
+++ src/main/java/com/rkweber/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.rkweber.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user") //corresponde a uma coleção dentro do banco (collection = 'user') opcional, documetno pega o nome da classe e deixa minuscula
@@ -14,6 +17,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true) // referencia uma coleção, e lazy=true significa que os posts vao ser acessados quando eu quiser e não quando eu quiser ver os ussuarios(vem so os dados basicos)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User(){}
 
@@ -47,11 +53,19 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -63,4 +77,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
 }
